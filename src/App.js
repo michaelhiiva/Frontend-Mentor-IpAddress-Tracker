2d207e58d8f03ddd809305d42d8cece5a05f6410
Michael Hiiva <michaelhiiva@gmail.com>
2023-08-07 19:18:28 -0700

    updated: Created Basic Template for Application.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import AppSearchBar from './AppSearchBar.js';
+import AppSearchResultsSummary from './AppSearchResultSummary.js';
+import AppMapSearchResult from './AppMapSearchResult.js';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h2>IP Address Tracker</h2>
+        <AppSearchBar />
       </header>
+      <main>
+        <AppSearchResultsSummary />
+        <AppMapSearchResult />
+      </main>
     </div>
   );
 }
 
-export default App;
